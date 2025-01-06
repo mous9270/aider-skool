07010422ded1a13cf8e34e425c5ec5ab422ab909
mous9270 <mous9270222@gmail.com>
2025-01-06 11:42:06 +0530

    logo changed to playto

--- components/logo.tsx
+++ components/logo.tsx
@@ -11,12 +11,13 @@ export const Logo = ({
     className
 }: LogoProps) => {
     return (
-        <div className={cn("cursor-pointer font-bold text-5xl" && className)}>
-            <span className="text-red-500">s</span>
-            <span className="text-blue-500">k</span>
-            <span className="text-green-500">u</span>
-            <span className="text-yellow-500">u</span>
-            <span className="text-purple-500">l</span>
+        <div className={cn("cursor-pointer ml-80 font-bold text-5xl" && className)}>
+            <span className="text-red-500">p</span>
+            <span className="text-blue-500">l</span>
+            <span className="text-green-500">a</span>
+            <span className="text-yellow-500">y</span>
+            <span className="text-purple-500">t</span>
+            <span className="text-purple-500">o</span>
         </div>
     );
 }
