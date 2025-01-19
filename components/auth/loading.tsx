faaeb8ce60fe4c386b918e081289afdbc2cc3fb2
mous9270 <mous9270222@gmail.com>
2025-01-19 09:29:24 +0530

    loading is updated

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export const Loading = () => {
     return (
-        <main className="flex w-full h-full items-center justify-center bg-white">
+        <main className="flex w-full h-full items-center justify-center bg-background">
             <Image
                 src={"/logo.svg"}
                 alt="logo"
