192848399290cc149e33a0dccf718590775f47e4
mous9270 <mous9270222@gmail.com>
2025-02-11 11:26:07 +0530

    loading image replaced with playto logo

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -4,7 +4,7 @@ export const Loading = () => {
     return (
         <main className="flex w-full h-full items-center justify-center bg-background">
             <Image
-                src={"/logo.svg"}
+                src={"/logo.png"}
                 alt="logo"
                 width={200}
                 height={200}
